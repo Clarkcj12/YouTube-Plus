a31c93438f9c7a9494e1a1b174157b21835133d0
Corey Clark <Clarkcj12@users.noreply.github.com>
2015-03-17 13:52:50 -0400

    Update YouTube.java
    Added permissions, and broadcast messages

--- src/me.clarkcj.youtube/YouTube.java
+++ src/me.clarkcj.youtube/YouTube.java
@@ -2,6 +2,7 @@ package me.Clarkcj.youtube;
 
 import java.util.logging.Logger;
 
+import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
@@ -19,25 +20,53 @@ public class Youtube extends JavaPlugin{
 		this.logger.info(pdfFile.getName() + " Has Been Disabled!");
     
     }
+    
     @Override
     public void onEnable(){
+    	this.saveDefaultConfig();
     	PluginDescriptionFile pdfFile = this.getDescription();
     	this.logger.info(pdfFile.getName() + " Version " + pdfFile.getVersion() + " Has Been Enabled!");
-    	getConfig().options().copyDefaults(true);
-    	saveConfig();
-  
     }
     
 	public boolean onCommand(CommandSender sender, Command cmd, String commandLabel, String[] args){
-		Player player = (Player) sender;
 		if (cmd.getName().equalsIgnoreCase("YouTube")){
-			player.sendMessage(ChatColor.GOLD + "Our Server's YouTube Channel is: "  + ChatColor.DARK_AQUA + getConfig().getString("YTChannel"));
-		}else if (cmd.getName().equalsIgnoreCase("yt")){
-			player.sendMessage(ChatColor.GOLD + " Our Server's YouTube Channel is: " + ChatColor.DARK_AQUA + getConfig().getString("YTChannel"));
-		}else if (cmd.getName().equalsIgnoreCase("Twitch")){			
-			player.sendMessage(ChatColor.GOLD + "The Server's Twitch Channel is: " + ChatColor.DARK_AQUA + getConfig().getString("Twitch"));
+			if (args.length >= 2) {
+				if ( sender instanceof Player) {
+					Player p = ((Player)sender);
+					if ( p.hasPermission("youtube.record") || p.isOp() ) {
+						if ( args[0].equalsIgnoreCase("record") ) {
+							if ( args[1].equalsIgnoreCase("-server")) {
+								Bukkit.broadcastMessage(getConfig().getString("prefix").replace('&', '§') + " Our Server is recording at §3 " + getConfig().getString("YTChannel") + "!");
+							} else {
+								Bukkit.broadcastMessage(getConfig().getString("prefix").replace('&', '§') + " " + sender.getName() + " is recording our server at " + args[1] + "!");
+							}
+						}
+					}
+				}
+			} else {
+				sender.sendMessage(ChatColor.GOLD + "Our Server's YouTube Channel is: "  + ChatColor.DARK_AQUA + getConfig().getString("YTChannel"));
+			}
+			return true;
+		} else if (cmd.getName().equalsIgnoreCase("Twitch")){
+			if (args.length >= 2) {
+				if ( sender instanceof Player) {
+					Player p = ((Player)sender);
+					if ( p.hasPermission("twith.stream") || p.isOp() ) {
+						if ( args[0].equalsIgnoreCase("stream") ) {
+							if ( args[1].equalsIgnoreCase("-server")) {
+								Bukkit.broadcastMessage(getConfig().getString("prefix").replace('&', '§') + "Our Server is streaming at: §3 " + getConfig().getString("Twitch") + "!");
+							} else {
+								Bukkit.broadcastMessage(getConfig().getString("prefix").replace('&', '§') + " " + sender.getName() + " is streaming our server at " + args[1] + "!");
+							}
+						}
+					}
+				}
+			} else {
+				sender.sendMessage(ChatColor.GOLD + "The Server's Twitch Channel is: " + ChatColor.DARK_AQUA + getConfig().getString("Twitch"));
+			}
+			return true;
+		} else {
+			return false;
 		}
-			
-		return false;
 	}
 }
